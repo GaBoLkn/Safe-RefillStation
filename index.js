f9cf7e2bddb2e8d15dc7641ee916f4fecc8805ab
GaBoLkn <gabrielsanchez040506@gmail.com>
2023-10-02 08:47:59 -0300

    js function

---
+++ index.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Función para cambiar el estado y los colores del div "alert"
+    function cambiarEstado(estado) {
+        var alertDiv = document.querySelector(".alert");
+        var circle = alertDiv.querySelector(".circle");
+        var h3 = alertDiv.querySelector("h3");
+
+        // Cambiar el texto y el color del círculo
+        circle.style.backgroundColor = estado === "Activa" ? "green" : "red";
+        h3.textContent = estado;
+
+        // Cambiar el color del texto "Activa" o "Inactiva"
+        h3.style.color = estado === "Activa" ? "green" : "red";
+    }
+
+    // Esta función se puede llamar cuando llega un dato del ESP8266
+    function recibirDatoDesdeESP8266(dato) {
+        // Llama a la función cambiarEstado con el nuevo estado
+        cambiarEstado(dato);
+    }
+
+    // Ejemplo: Llama a recibirDatoDesdeESP8266 con el estado "Inactiva" cuando llega un dato
+    recibirDatoDesdeESP8266("Inactiva");
+});
